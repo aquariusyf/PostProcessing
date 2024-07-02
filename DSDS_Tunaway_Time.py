cd67d502e81b8da1e3546028ec00d4a3208cd8d7
Yifan.Z <aquariusyf@gmail.com>
2024-07-02 16:29:30 -0700

    Update DSDS_Tunaway_Time Add TA count and total TA time to result

--- DSDS_Tunaway_Time.py
+++ DSDS_Tunaway_Time.py
@@ -4,7 +4,6 @@
 
 from PostProcessingUtils import PostProcessingUtils
 from FilterMask import *
-from openpyxl import Workbook
 from datetime import datetime
 import pandas as pd
 import sys
@@ -20,14 +19,21 @@ RE_DTA = re.compile(r'.*NR5GML1_QSH_EVENT_MSIM_(DTA).*')
 RE_QDTA = re.compile(r'.*NR5GML1_QSH_EVENT_MSIM_(QDTA).*')
 RE_TA_TIME = re.compile(r'.*ta_time: ([\d]+) ms.*')
 
-AVG_LTA_TIME = 'avg_lta_time'
-AVG_DTA_TIME = 'avg_dta_time'
-AVG_QDTA_TIME = 'avg_qdta_time'
+AVG_LTA_TIME = 'AVG LTA Time (ms)'
+AVG_DTA_TIME = 'AVG DTA Time (ms)'
+AVG_QDTA_TIME = 'AVG QDTA Time (ms)'
+NUM_LTA = 'Num of LTA'
+NUM_DTA = 'Num of DTA'
+NUM_QDTA = 'Num of QDTA'
+TOTAL_LTA_TIME = 'Total LTA Time (ms)'
+TOTAL_DTA_TIME = 'Total DTA Time (ms)'
+TOTAL_QDTA_TIME = 'Total QDTA Time (ms)'
 KPI = 'kpi'
 LOGNAME = 'logname'
 
 ARGV = sys.argv
 ARGV.append('-qtrace')
+ARGV.append('-sub1')
 
 tuneawayTime = PostProcessingUtils()
 tuneawayTime.getArgv(ARGV)
@@ -38,7 +44,7 @@ tuneawayTime.scanWorkingDir('_flt_text.txt', 'Tunaway_Time')
 tuneawayTime.initLogPacketList()
 
 
-dict_ta_time = {KPI: [AVG_LTA_TIME, AVG_DTA_TIME, AVG_QDTA_TIME]}
+dict_ta_time = {KPI: [AVG_LTA_TIME, NUM_LTA, TOTAL_LTA_TIME, AVG_DTA_TIME, NUM_DTA, TOTAL_DTA_TIME, AVG_QDTA_TIME, NUM_QDTA, TOTAL_QDTA_TIME]}
 all_log_pkt = tuneawayTime.getLogPacketList()
 for logname, logs in all_log_pkt.items():
     dict_ta_time[logname] = []
@@ -64,15 +70,27 @@ for logname, logs in all_log_pkt.items():
                 count_qdta += 1
     if count_lta != 0:             
         dict_ta_time[logname].append(int(sum_lta/count_lta))
+        dict_ta_time[logname].append(count_lta)
+        dict_ta_time[logname].append(sum_lta)
     else:
+        dict_ta_time[logname].append('NA')
+        dict_ta_time[logname].append(count_lta)
         dict_ta_time[logname].append('NA')
     if count_dta != 0:
         dict_ta_time[logname].append(int(sum_dta/count_dta))
+        dict_ta_time[logname].append(count_dta)
+        dict_ta_time[logname].append(sum_dta)
     else:
+        dict_ta_time[logname].append('NA')
+        dict_ta_time[logname].append(count_dta)
         dict_ta_time[logname].append('NA')
     if count_qdta != 0:
         dict_ta_time[logname].append(int(sum_qdta/count_qdta))
+        dict_ta_time[logname].append(count_qdta)
+        dict_ta_time[logname].append(sum_qdta)
     else:
+        dict_ta_time[logname].append('NA')
+        dict_ta_time[logname].append(count_qdta)
         dict_ta_time[logname].append('NA')
 
 df = pd.DataFrame(dict_ta_time)
